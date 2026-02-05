2165a05d577cf355e455444bf949811921553116
LemonFrz <lmao.pole@gmail.com>
2026-02-05 16:08:41 +0700

    Force update - Remove light mode

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting Deployment...
+echo --------------------------------------------
+git add .
+git commit -m "Force update - Remove light mode"
+git push
+echo --------------------------------------------
+echo Done! Your code is on GitHub.
+echo.
+echo Please wait 2-3 minutes for the website to update.
+echo Then checkout your site in an INCOGNITO window.
+pause
